416000914f6ec908ac140f4b08319a94c5c3f092
vedran.semenski <vedran.semenski@ua.pt>
2015-04-28 14:27:36 +0000

    Updated and rest moved to Helper lib

--- EclipseWorkspace/AccessControlPEP/src/smartie/accesscontrol/pep/Main.java
+++ EclipseWorkspace/AccessControlPEP/src/smartie/accesscontrol/pep/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO make test function for testing the policies and fetching of SensorData
+		
+		
+//		public static void main(String[] args) { 
+//			RestClient client = new RestClient();
+//			client.evaluateRequest("");
+//		}
 	}
 }
