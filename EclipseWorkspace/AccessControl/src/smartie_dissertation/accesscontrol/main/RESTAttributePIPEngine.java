2f67b9bf15cb3b04cf64e0fe8f283e21e612a7bd
vedran.semenski <vedran.semenski@ua.pt>
2015-05-21 16:41:58 +0000

    Working after testing

--- EclipseWorkspace/AccessControl/src/smartie_dissertation/accesscontrol/main/RESTAttributePIPEngine.java
+++ EclipseWorkspace/AccessControl/src/smartie_dissertation/accesscontrol/main/RESTAttributePIPEngine.java
@@ -1,16 +1,31 @@
 package smartie_dissertation.accesscontrol.main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
 
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//import smartie.accesscontrol.pep.RESTPEP;
+import smartie_dissertation.helperlib.XACML.AttributeFactory;
+import smartie_dissertation.helperlib.XACML.SmartieXACML;
+import smartie_dissertation.helperlib.log.MyLog;
+import smartie_dissertation.helperlib.rest.RestClient;
+import smartie_dissertation.helperlib.rest.RestResponse;
+
 import com.att.research.xacml.api.Attribute;
 import com.att.research.xacml.api.pip.PIPException;
 import com.att.research.xacml.api.pip.PIPFinder;
 import com.att.research.xacml.api.pip.PIPRequest;
 import com.att.research.xacml.api.pip.PIPResponse;
+import com.att.research.xacml.util.FactoryException;
 
 public class RESTAttributePIPEngine extends SmartiePIPEngine {
 	//TODO make rest fetching service
@@ -27,15 +42,48 @@ public class RESTAttributePIPEngine extends SmartiePIPEngine {
 	}
 	
 	@Override
-	protected void loadAttributesRequired() {
-//		this.listAttributesRequired	= new ArrayList<Attribute>();
+	protected void loadAttributesRequired() {		
+		this.listAttributesRequired	= new ArrayList<Attribute>();
+		this.listAttributesRequired	= new ArrayList<Attribute>();
+		AttributeFactory attributeFactory = AttributeFactory.getInstance();
+//		try {
+//			//TODO: mention that the attibute is an ID attribute
+////			this.listAttributesRequired.add(attributeFactory.createAttribute(SmartieXACML.URN_CATEGORY_ENVIRONMENT, ENVIRONMENT_ATTRIBUTE_CURRENT_DATE, SmartieXACML.DATATYPE_DATE));
+//		} catch (FactoryException e) {
+//			MyLog.log("Error loading provided attributes", MyLog.logMessageType.ERROR, this.getName(), e);
+//		}
 	}	
 	@Override
 	protected void loadAttributesProvided(){
-//		Set<PIPRequest> returnSet = new HashSet<PIPRequest>();
-//		this.listAttributesProvided = new ArrayList<Attribute>();
-//		for(Attribute attribute : this.listAttributesLoaded){
-//			this.listAttributesProvided.add(attribute);
+		this.listAttributesProvided	= new ArrayList<Attribute>();	
+		
+		//TODO: fetch the attribute values and make attributes from that
+		
+		
+//		RestClient client = new RestClient(REST_BASE_URI);		
+////		SecureRestClient client = new 		
+//		HashMap<String, String> formData = new HashMap<>();
+//		formData.put("request", request);
+//		
+//		JSONObject jsonResponse = client.postRequest(REST_EVALUATE_REQUEST_FUNCTION, formData);
+//		RestResponse response;
+//		boolean result = false;
+//		try {
+//			response = RestResponse.JsonToRestResponse(jsonResponse);
+//			result = response.result;
+//		} catch (JSONException e) {
+//			MyLog.log("Rest service resposnse JSONObject doesn't have required fields", MyLog.logMessageType.ERROR, RESTPEP.class.getName(), e);
+//			result = false;
+//		}
+		
+		
+//		AttributeFactory attributeFactory = AttributeFactory.getInstance();
+//		try {
+//			this.listAttributesProvided.add(attributeFactory.createAttribute(SmartieXACML.URN_CATEGORY_ENVIRONMENT, ENVIRONMENT_ATTRIBUTE_CURRENT_DATE, SmartieXACML.DATATYPE_DATE));
+//			this.listAttributesProvided.add(attributeFactory.createAttribute(SmartieXACML.URN_CATEGORY_ENVIRONMENT, ENVIRONMENT_ATTRIBUTE_CURRENT_TIME, SmartieXACML.DATATYPE_TIME));
+//			this.listAttributesProvided.add(attributeFactory.createAttribute(SmartieXACML.URN_CATEGORY_ENVIRONMENT, ENVIRONMENT_ATTRIBUTE_CURRENT_DATETIME, SmartieXACML.DATATYPE_DATETIME));
+//		} catch (FactoryException e) {
+//			MyLog.log("Error loading provided attributes", MyLog.logMessageType.ERROR, this.getName(), e);
 //		}
 	}
 	
